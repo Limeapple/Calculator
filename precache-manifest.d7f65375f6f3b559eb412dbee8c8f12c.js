ab8a0dcb8b5eab94e74baedf596579e897701a42
Limeapple <Kevinliu54321@gmail.com>
2019-05-05 17:51:35 -0500

    Updates

---
+++ precache-manifest.d7f65375f6f3b559eb412dbee8c8f12c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "655796775b2213c54d6f",
+    "url": "/Calculator/static/css/main.2b6b9009.chunk.css"
+  },
+  {
+    "revision": "655796775b2213c54d6f",
+    "url": "/Calculator/static/js/main.65579677.chunk.js"
+  },
+  {
+    "revision": "c2a97d8d426c7554df3f",
+    "url": "/Calculator/static/js/runtime~main.c2a97d8d.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Calculator/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b9c13310433af4251b508b8ec2207eb2",
+    "url": "/Calculator/index.html"
+  }
+];
